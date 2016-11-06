5f85ec342a5cbde4b2f1dacce50ee8c52d3eb5d7
mike <mike@fusebuilder.net>
2016-11-06 01:19:14 -0700

    working login, logout, signup, change profile

--- controllers/member.js
+++ controllers/member.js
@@ -1,50 +1,81 @@
-var express = require('express');
-var router = express.Router();
-var auth = require('../auth');
-var member = require('../models/mw.member');
+var express    = require('express');
+var router     = express.Router();
+var auth       = require('../auth');
+var member     = require('../models/mw.member');
+var crypto     = require('crypto');
 
 /**
  *Shows the login form
  */
 router.get('/login', function(req, res){
-	res.render("member_login", {role: req.session.role, title: " login"});
+	res.render("member_login", {role: req.session.role, errors: [], title: "Member Login"});
 });
 
 /**
  *Logs the user in
  */
-router.post('/login', function(req, res){
-	
-	res.redirect("/member/trades");
+router.post('/login', member.getByLogin, function(req, res){
+	if ( !res.member ) {
+		res.render("member_login", {role: req.session.role, errors: ['Invalid email or password'], title: "Member Login"});
+	} else {
+		req.session.role = 'member';
+		req.session.memberInfo = res.member;
+		res.redirect("/member/trades");
+	}
 });
 
 /**
  *Shows the list of books trades initiated by the user, and a list of book trades directed at the user. User can accept/reject trades, and view the profile of the other party in the trade
  */
 router.get('/trades', auth.requires('member'), function(req, res){
-	res.render("member_trades", {role: req.session.role, title: " trades"});
+	res.render("member_trades", {role: req.session.role, title: "My Trades"});
 });
 
 /**
  *Displays the form to allow a user to change their first/last name, city and state
  */
 router.get('/profile', auth.requires('member'), function(req, res){
-	res.render("member_profile", {role: req.session.role, title: " profile"});
+	var message = ( typeof req.query.changed != 'undefined' ) ? 'Profile changed successfullly' : '';
+	res.render("member_profile", {role: req.session.role, title: "My Profile", message: message, errors: [], memberInfo: req.session.memberInfo});
 });
 
 /**
  *Updates a member profile
  */
 router.post('/profile', auth.requires('member'), function(req, res){
-	
-	res.redirect("/member/profile?changed");
+	var passwordRequired    = ( req.body.password.length > 0 ),
+		errors              = member.validateInfo(req.body, req.body, false, passwordRequired);
+	if ( errors.length > 0 ) {
+		res.render("member_profile", {role: req.session.role, title: "My Profile", message: '', errors: errors, memberInfo: req.body});
+	} else {
+		var memberInfo = {
+			'email'		: req.session.memberInfo.email,
+			'firstname'	: req.body.firstname,
+			'lastname'	: req.body.lastname,
+			'city'		: req.body.city,
+			'state'		: req.body.state
+		};
+		if ( passwordRequired ) {
+			memberInfo.pwsalt = new Date().getTime().toString(36);
+			var hash = crypto.createHmac('sha512', memberInfo.pwsalt);
+			hash.update(req.body.password);
+			memberInfo.pwhash = hash.digest('hex');
+		}
+		member.update(memberInfo, function(){
+			delete memberInfo.pwsalt;
+			delete memberInfo.pwhash;
+			req.session.memberInfo = memberInfo;
+			res.redirect("/member/profile?changed");
+		});
+		return;
+	}
 });
 
 /**
  *Displays a list of books owned by the user, with administrative links to manage them
  */
 router.get('/books', auth.requires('member'), function(req, res){
-	res.render("member_books", {role: req.session.role, title: " books"});
+	res.render("member_books", {role: req.session.role, title: "My Books"});
 });
 
 /**
@@ -59,7 +90,7 @@ router.post('/books/:id/remove', auth.requires('member'), function(req, res){
  *Displays a form interface enabling the user to make changes to a single book in their list
  */
 router.get('/books/:id/edit', auth.requires('member'), function(req, res){
-	res.render("member_books_id_edit", {role: req.session.role, title: " books :id edit"});
+	res.render("member_books_id_edit", {role: req.session.role, title: "Edit Book"});
 });
 
 /**
@@ -74,7 +105,7 @@ router.post('/books/:id/edit', auth.requires('member'), function(req, res){
  *Displays a form interface to enable the user to add a new book to their list
  */
 router.get('/books/new', auth.requires('member'), function(req, res){
-	res.render("member_books_new", {role: req.session.role, title: " books new"});
+	res.render("member_books_new", {role: req.session.role, title: "Add a Book"});
 });
 
 /**
@@ -89,8 +120,62 @@ router.post('/books/new', auth.requires('member'), function(req, res){
  *Logs the current user out of their session
  */
 router.get('/logout', function(req, res){
-	
+	delete req.session.role;
+	delete req.session.memberInfo;
 	res.redirect("/books");
+});
+
+/**
+ *Form interface for creating a new membership
+ */
+router.get('/signup', function(req, res){
+	res.render("member_signup", {role: req.session.role, errors: [], title: "Member Signup", memberInfo: {
+		email: '',
+		password: '',
+		password_confirm: '',
+		firstname: '',
+		lastname: '',
+		city: '',
+		state: ''
+	}});
+});
+
+/**
+ *Create a new membership
+ */
+router.post('/signup', member.getByEmail('body'), function(req, res){
+	var memberInfo = {
+			'email'		: req.body.email,
+			'pwsalt'	: new Date().getTime().toString(36),
+			'firstname'	: req.body.firstname,
+			'lastname'	: req.body.lastname,
+			'city'		: req.body.city,
+			'state'		: req.body.state
+		},
+		errors = member.validateInfo(memberInfo, req.body),
+		emailUsed = false;
+
+	if ( memberInfo.email.length > 0 && res.member ) {
+		emailUsed = true;
+		memberInfo.email = '';
+		errors.push('Email is not available; please try another one');
+	}
+	if ( errors.length === 0 ) {
+		var hash = crypto.createHmac('sha512', memberInfo.pwsalt);
+		hash.update(req.body.password);
+		memberInfo.pwhash = hash.digest('hex');
+		return member.create(memberInfo, function(){
+			delete memberInfo.pwsalt;
+			delete memberInfo.pwhash;
+			req.session.role = 'member';
+			req.session.memberInfo = memberInfo;
+			res.redirect("/member/books");
+		});
+	} else {
+		memberInfo.password = ( emailUsed ) ? '' : req.body.password;
+		memberInfo.password_confirm = ( emailUsed ) ? '' : req.body.password_confirm;
+		res.render("member_signup", {role: req.session.role, errors: errors, title: "Member Signup", memberInfo: memberInfo});
+	}
 });
 
 module.exports = router;
--- models/mw.member.js
+++ models/mw.member.js
@@ -1,10 +1,13 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
+var crypto      = require('crypto');
+var validator   = require('validator');
 
 module.exports = (function(){
 	var memberModel = mongoose.model('booksMember', new Schema({
 		'email'		: String,
-		'password'	: String,
+		'pwsalt'	: String,
+		'pwhash'	: String,
 		'firstname'	: String,
 		'lastname'	: String,
 		'city'		: String,
@@ -13,14 +16,46 @@ module.exports = (function(){
 
 	var objReturn = {};
 
+	objReturn.create = function(memberInfo, callback) {
+		var member = new memberModel(memberInfo).save(function(err, doc, rowsaffected){
+			callback();
+		});
+	};
+
+	objReturn.update = function(memberInfo, callback) {
+		var member = memberModel.findOne({email: memberInfo.email}).exec(function(err, member){
+			if ( err ) return;
+			member.firstname = memberInfo.firstname;
+			member.lastname = memberInfo.lastname;
+			member.city = memberInfo.city;
+			member.state = memberInfo.state;
+			if ( memberInfo.pwsalt && memberInfo.pwhash ) {
+				member.pwsalt = memberInfo.pwsalt;
+				member.pwhash = memberInfo.pwhash;
+			}
+			member.save(function(err, doc, rowsaffected){
+				callback();
+			});
+		});
+	};
+
+	objReturn.findOne = function(email, callback) {
+		memberModel.findOne({email: email}).exec(function(err, result){
+			if ( err ) result = null;
+			callback(result);
+		});
+	};
+
 	objReturn.getByLogin = function(req, res, next) {
-		memberModel.findOne({'email': req.body.email}).exec(function(err, result){
+		memberModel.findOne({email: req.body.email}).exec(function(err, result){
 			if ( !err && result && 'pwsalt' in result ) { 
 				var hash = crypto.createHmac('sha512', result.pwsalt);
 				hash.update(req.body.password);
 				if ( result.pwhash == hash.digest('hex') ) {
+					delete result.pwsalt;
+					delete result.pwhash;
 					res.member = result;
-					next();
+					return next();
 				}
 			}
 			res.member = null;
@@ -28,11 +63,35 @@ module.exports = (function(){
 		});
 	};
 
-	objReturn.getByUsername = function(req, res, next) {
-		memberModel.findOne({'email': req.session.email}).exec(function(err, result){
-			res.member = ( err ) ? null : result;
-			next();
-		});
+	objReturn.getByEmail = function(scope) {
+		return function(req, res, next) {
+			objReturn.findOne({email: req[scope].email}).exec(function(result){
+				res.member = result;
+				next();
+			});
+		}
 	}
+
+	objReturn.validateInfo = function(memberInfo, postData, requireEmail, requirePassword) {
+		if ( arguments.length < 3 ) requireEmail = true;
+		if ( arguments.length < 4 ) requirePassword = true;
+		var errors = [];
+		if ( memberInfo.firstname.length === 0 ) { errors.push('First Name is required'); }
+		if ( memberInfo.lastname.length === 0 ) { errors.push('Last Name is required'); }
+		if ( memberInfo.city.length === 0 ) { errors.push('City is required'); }
+		if ( memberInfo.state.length === 0 ) { errors.push('State is required'); }
+		if ( requireEmail ) {
+			if ( memberInfo.email.length === 0 ) { errors.push('Email is required'); }
+			if ( !validator.isEmail(memberInfo.email) ) { errors.push('Please use a valid email address'); }
+		}
+		if ( requirePassword ) {
+			if ( postData.password.length === 0 ) { errors.push('Password is required'); }
+			if ( postData.password_confirm.length === 0 ) { errors.push('Confirm Password is required'); }
+			if ( postData.password != postData.password_confirm ) { errors.push('Password and Confirm Password don\'t match') }
+		}
+		return errors;
+	}
+
+	return objReturn;
 	
 })();
